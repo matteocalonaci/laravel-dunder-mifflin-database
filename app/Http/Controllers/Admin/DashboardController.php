028efae99d54a404986d81c92979663756aef265
MatteoCalonaci <matteo.calonaci.1994@gmail.com>
2025-03-06 17:16:06 +0100

    la dashboard reindirizza alla pagina principale

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,12 +3,17 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('admin.dashboard');
+        // Reindirizza in base al ruolo dell'utente
+        if (Auth::user()->role === 'admin') {
+            return redirect()->route('admin.employees.index'); // Reindirizza a Gestisci Dipendenti
+        } else {
+            return redirect()->route('employee.profile'); // Reindirizza al Profilo
+        }
     }
 }
